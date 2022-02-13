42f23616367e8c52d922ffb7a0eba7a1d04ae4b8
Jakub Sordyl <jakubsordyl1@gmail.com>
2022-02-13 10:45:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -73,7 +73,7 @@ namespace math_utilities {
 }  // namespace math_utilities
 
 namespace sulution_constants {
-  inline constexpr double G = 6.6743e-11;
+  inline constexpr double G = 1;
   inline constexpr auto solution_range = Range{ 0.0, 3.0 };
   inline constexpr auto p = [](const auto x) {
     return (x > 1 && x <= 2) ? 1 : 0;
@@ -95,9 +95,9 @@ auto get_base_functions(const double width) {
   const auto e = [width, &getRanges](const auto i) {
     return [width, i, ranges = getRanges(i)](const auto x) {
       if (x > ranges[0] && x <= ranges[1])
-        return (ranges[0] - x) / width;
+        return (x - ranges[0]) / width;
       else if (x > ranges[1] && x < ranges[2])
-        return (x - ranges[2]) / width;
+        return (ranges[2] - x) / width;
       else
         return 0.0;
     };
@@ -117,23 +117,23 @@ auto get_base_functions(const double width) {
   return std::pair{ e, de };
 }
 
-auto getMatrixA(const int elements_count, const auto de, const double width) {
-  std::vector<std::vector<double>> A(elements_count + 1, std::vector<double>(elements_count + 1, 0.0f));
+auto getMatrixB(const int elements_count, const auto de, const double width) {
+  std::vector<std::vector<double>> B(elements_count + 1, std::vector<double>(elements_count + 1, 0.0f));
 
-  A[0][0] = A[elements_count][elements_count] = 1.0;
+  B[0][0] = B[elements_count][elements_count] = 1.0;
   for (const auto i : std::views::iota(1, elements_count)) {
-    for (const auto j : std::views::iota(i - 1, i + 2)) {  // abs(i - j) <= 1
+    for (const auto j : std::views::iota(i - 1, i + 2)) {
       if (j == 0 || j == elements_count)
         continue;
 
-      A[i][j] = A[j][i]
+      B[i][j] = B[j][i]
           = -math_utilities::integrate([&de, i, j](const auto x) { return de(i)(x) * de(j)(x); },
                                        (i == j) ? Range{ (i - 1) * width, (i + 1) * width }
                                                 : Range{ std::min(i, j) * width, std::max(i, j) * width });
     }
   }
 
-  return A;
+  return B;
 }
 
 auto getMatrixL(const int elements_count, const auto de, const auto e, const double width) {
@@ -141,17 +141,17 @@ auto getMatrixL(const int elements_count, const auto de, const auto e, const dou
 
   for (const auto i : std::views::iota(1, elements_count)) {
     const auto range = Range{ (i - 1) * width, (i + 1) * width };
-    const auto l1 = [&e, i](const auto x) {
+    const auto l = [&e, i](const auto x) {
       return 4 * std::numbers::pi * sulution_constants::G * sulution_constants::p(x) * e(i)(x);
     };
-    const auto l2 = [&de, i](const auto x) {
+    const auto bu = [&de, i](const auto x) {
       return sulution_constants::dshifted(x) * de(i)(x);
     };
 
-    L[i] = math_utilities::integrate(l1, Range{ range.begin, range.end - width })
-           + math_utilities::integrate(l1, Range{ range.begin + width, range.end })
-           + math_utilities::integrate(l2, Range{ range.begin, range.end - width })
-           + math_utilities::integrate(l2, Range{ range.begin + width, range.end });
+    L[i] = math_utilities::integrate(l, Range{ range.begin, range.end - width })
+           + math_utilities::integrate(l, Range{ range.begin + width, range.end })
+           + math_utilities::integrate(bu, Range{ range.begin, range.end - width })
+           + math_utilities::integrate(bu, Range{ range.begin + width, range.end });
   }
 
   return L;
@@ -162,7 +162,7 @@ std::vector<double> MES(const int elements_count) {
       = (sulution_constants::solution_range.end - sulution_constants::solution_range.begin) / elements_count;
   const auto [e, de] = get_base_functions(width);
 
-  return math_utilities::gaussJordan(getMatrixA(elements_count, de, width), getMatrixL(elements_count, de, e, width));
+  return math_utilities::gaussJordan(getMatrixB(elements_count, de, width), getMatrixL(elements_count, de, e, width));
 }
 
 template<typename T>
@@ -181,7 +181,7 @@ void draw_chart(std::string_view file_output_name, const int points_count, const
 
   std::ofstream file(file_output_name.data());
 
-  for (const auto [i, x] : std::views::iota(0, points_count)
+  for (const auto [i, x] : std::views::iota(0, points_count + 1)
                                | std::views::transform([width](const auto i) { return std::pair(i, width * i); })) {
     file << x << ' ' << solutionMatrix[i] + sulution_constants::shifted(x) << '\n';
   }
